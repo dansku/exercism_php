907dedf0f3ff80de5f41685a28fc7df07002964d
Daniel Andrade <daniel@danielandrade.net>
2020-04-08 17:47:00 +0100

    Add hamming

---
+++ hamming/hamming.php
@@ -0,0 +1,18 @@
+<?php
+
+// Calculate the Hamming distance between two DNA  strands
+function distance(string $strandA, string $strandB) : int
+{
+    if(strlen($strandA) != strlen($strandB)){
+        throw new InvalidArgumentException('DNA strands must be of equal length.');
+    }
+
+    $distance = 0;
+    for($i=0; $i<strlen($strandA);$i++){
+        if($strandA[$i] != $strandB[$i]) {
+            $distance++;
+        }
+    }
+
+    return $distance;
+}
